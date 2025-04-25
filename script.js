d54bcefba763ce26d39f57b4c435dacc264f9d52
Mario Vega <marioalexgarcia86@gmail.com>
2025-04-24 18:20:57 -0600

    Finalización de Game of Rol

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const locations = [
     {
         name: "kill monster",
         "button text": ["Go to town square", "Go to town square", "Go to town square"],
-        "button functions": [goTown, goTown, goTown],
+        "button functions": [goTown, goTown, easterEgg],
         text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
     },
     {
@@ -263,4 +263,26 @@ function pickTwo() {
 function pickEight() {
     pick(8);
 }
-
+function  pick(guess){
+    const numbers = [];
+    while (numbers.length < 10) {
+        numbers.push(Math.floor(Math.random() * 11));
+    }
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+    for (let i = 0; i < 10; i++) {
+    text.innerText = numbers[i]+"\n";
+    }
+    if(numbers.includes(guess)){
+        text.innerText += "Right! You win 20 gold!";
+        gold += 20;
+        goldText.innerText = gold;
+    }
+    else{
+        text.innerText += "Wrong! you lose 10 health!";
+        health -= 10;
+        healthText.innerText = health;
+        if(health <=0){
+            lose();
+        }
+    }
+}
